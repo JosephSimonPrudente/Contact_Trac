3f3c9592d80260426b4d0174fd1dc1e038389d08
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-11-12 22:40:19 +0800

    Allow user to select item in the menu (check if valid)

--- Dictionaries_Manipulation.py
+++ Dictionaries_Manipulation.py
@@ -5,6 +5,9 @@ print("2 -> Search")
 print("3 -> Exit")
 
 #Allow user to select item in the menu (check if valid)
+user = int(input("Select from the Menu:"))
+if user > 3:
+    print("Invalid")
 
 #Perform the selected option
 #- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
