f79897fb86c9f0b279cde5d1202e7d3c43f403d5
JosephSimonPrudente <prudentejoseph3@gmail.com>
2022-11-12 22:29:48 +0800

    Menu Options

---
+++ Dictionaries_Manipulation.py
@@ -0,0 +1,17 @@
+#Display Menu options
+print("=====MENU=====")
+print("1 -> Add Item")
+print("2 -> Search")
+print("3 -> Exit")
+
+#Allow user to select item in the menu (check if valid)
+
+#Perform the selected option
+#- Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+				   #Use dictionary to store the info
+				   #Use the full name as key
+				   #The value is another dictionary of personal information
+
+#- Option 2: Search, ask full name then display the record
+
+#- Option 3: Ask the user if want to exit or retry.
